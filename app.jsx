66aadf4f8b143d1a454784350e7526c42beb5297
Idris143 <32055334+Idris143@users.noreply.github.com>
2017-09-20 00:00:54 +0530

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -9,14 +9,33 @@ class App extends React.Component {
     }
 
     this.UserList = this.UserList.bind(this);
+    this.setStateHandler = this.setStateHandler.bind(this);
 
+this.setDeleteHandler = this.setDeleteHandler.bind(this);
   }
   componentDidMount() {
     this.UserList();
   }
+ setStateHandler() {
+ 	alert(0)
+      var item = {
+        "url": "http://www.imdb.com/title/tt4937812/", 
+        "title": "PAMMAL K SAMBANTHAM", 
+        "imdb_id": "tt4937812", 
+        "type": "Film", 
+        "year": "2015"
+      };
+      var myArray = this.state.posts;
+      myArray.push(item)
+      console.log(myArray)
+      this.setState({posts: myArray})
+   };
+    setDeleteHandler() {
+ 	alert("idris")
 
+   };
   UserList() {
-
+	alert(1)
       fetch('http://www.theimdbapi.org/api/person?person_id=nm0352032').then(response => {
       return response.json();
     }).then(data => {
@@ -32,10 +51,12 @@ class App extends React.Component {
         <div>
           {
             this.state.posts.map((item, i) => {
-              return <Content item={item} key={i}/>
+              return <Content item={item} key={i}/> 
             })
           }
         </div>
+        <button onClick = {this.setStateHandler}>SET STATE</button>
+
       </div>
     );
   }
@@ -46,13 +67,20 @@ class Content extends React.Component {
     return (
       <div>
       <table>
+      <tbody>
 <tr>
 <td>{this.props.item.title}</td>
 <td>{this.props.item.type}</td>
+<td>{this.props.item.type}</td>
+<td><button onClick = {this.setDeleteHandler}>Delete</button></td>
+
+
 </tr>
+</tbody>
       </table>
         
       </div>
+
     );
   }
 }
